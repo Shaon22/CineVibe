5cd8974af8403ba0499424d9c0956a1cfc8fc0ba
shaon22 <shaonroy11@gmail.com>
2025-07-24 04:01:56 +0600

    added sidenav

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,28 +1,59 @@
-import { Clapperboard } from "lucide-react";
+import { Clapperboard, Home, ChevronRight, Tv, FileSpreadsheet, Radio } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Sidenav = () => {
-  const commonClasses = "px-20 py-2 border border-gray-500 rounded text-white hover:bg-gray-500";
+  const commonClasses =
+    "py-2 px-2 border border-gray-500 rounded text-white flex items-center justify-between gap-2 text-base font-bold hover:bg-gray-500 ";
+  const commonClasses2 = "flex items-center gap-2";
+
   return (
     <div>
-       <div className="flex justify-center mb-8 mt-5">
-            <div className="flex items-center space-x-3">
-              <div className="flex items-center justify-center w-12 h-12 bg-white rounded-lg">
-                <Clapperboard className="w-6 h-6 text-purple-600 fill-current" />
-              </div>
-              <h2 className="text-3xl font-bold text-white">CineVibe</h2>
-            </div>
+      <div className="flex justify-center mb-8 mt-5">
+        <div className="flex items-center space-x-3">
+          <div className="flex items-center justify-center w-12 h-12 bg-white rounded-lg">
+            <Clapperboard className="w-6 h-6 text-purple-600 fill-current" />
           </div>
+          <h2 className="text-3xl font-bold text-white">CineVibe</h2>
+        </div>
+      </div>
 
-      <div className="flex flex-col gap-5 p-5 text-center">
-        <Link className={commonClasses} to={'u'}>Home</Link>
-        <Link className={commonClasses} to={'/'}>Movies</Link>
-        <Link className={commonClasses} to={'/'}>Series</Link>
-        <Link className={commonClasses} to={'/'}>Blogs</Link>
-        <Link className={commonClasses} to={'/'}>Home</Link>
-        
-       
-    </div>
+      <div className="flex flex-col gap-5 p-5">
+        <div className={commonClasses}>
+          <div className={commonClasses2}>
+            <Home />
+            <Link to={"u"}>Home</Link>
+          </div>
+          <ChevronRight />
+        </div>
+         <div className={commonClasses}>
+          <div className={commonClasses2}>
+            <Clapperboard />
+            <Link to={"u"}>Movies</Link>
+          </div>
+          <ChevronRight />
+        </div>
+         <div className={commonClasses}>
+          <div className={commonClasses2}>
+            <Tv />
+            <Link to={"u"}>TV Series</Link>
+          </div>
+          <ChevronRight />
+        </div>
+        <div className={commonClasses}>
+          <div className={commonClasses2}>
+            <FileSpreadsheet />
+            <Link to={"u"}>Blogs</Link>
+          </div>
+          <ChevronRight />
+        </div>
+         <div className={commonClasses}>
+          <div className={commonClasses2}>
+            <Radio/>
+            <Link to={"u"}>Live Shows</Link>
+          </div>
+          <ChevronRight />
+        </div>
+      </div>
     </div>
   );
 };
