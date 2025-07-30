bd5f9b420ba6cd4fbe91d6e0bb90e93484ca3508
shaon22 <shaonroy11@gmail.com>
2025-07-31 02:53:36 +0600

    updated

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,28 +1,20 @@
-import {
-  Clapperboard,
-  Home,
-  ChevronRight,
-  Tv,
-  FileSpreadsheet,
-  Radio,
-} from "lucide-react";
-import { Link } from "react-router-dom";
+import { Clapperboard, Home, ChevronRight, Tv, FileSpreadsheet, Radio } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const Sidenav = () => {
   const commonClasses =
-    "py-2 px-2 border border-gray-500 rounded text-white flex items-center justify-between gap-2 text-base font-bold hover:bg-gray-500 ";
-  const commonClasses2 = "flex items-center gap-2";
+    "py-2 px-2 border border-gray-500 rounded text-white flex items-center justify-between gap-2 text-base font-bold hover:bg-gray-500 "
+  const commonClasses2 = "flex items-center gap-2"
 
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <div className="flex justify-center mb-8 mt-5">
         <div className="flex items-center space-x-2">
           <Clapperboard className="w-6 h-6 text-white" />
           <h2 className="text-3xl font-bold text-white">CineVibe</h2>
         </div>
       </div>
-
-      <div className="flex flex-col gap-5 p-5">
+      <nav className="flex flex-col gap-5 p-5 flex-1">
         <Link to={"/"}>
           <div className={commonClasses}>
             <div className={commonClasses2}>
@@ -32,7 +24,6 @@ const Sidenav = () => {
             <ChevronRight />
           </div>
         </Link>
-
         <Link to={"/allMovies"}>
           <div className={commonClasses}>
             <div className={commonClasses2}>
@@ -42,7 +33,6 @@ const Sidenav = () => {
             <ChevronRight />
           </div>
         </Link>
-
         <Link to={"/allSeries"}>
           <div className={commonClasses}>
             <div className={commonClasses2}>
@@ -52,7 +42,6 @@ const Sidenav = () => {
             <ChevronRight />
           </div>
         </Link>
-
         <Link to={"/allBlogs"}>
           <div className={commonClasses}>
             <div className={commonClasses2}>
@@ -62,8 +51,7 @@ const Sidenav = () => {
             <ChevronRight />
           </div>
         </Link>
-
-        <Link to={""}>
+        <Link to={"#"}>
           <div className={commonClasses}>
             <div className={commonClasses2}>
               <Radio />
@@ -72,9 +60,9 @@ const Sidenav = () => {
             <ChevronRight />
           </div>
         </Link>
-      </div>
+      </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Sidenav;
+export default Sidenav
