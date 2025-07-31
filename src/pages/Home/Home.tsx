592f0efc695ad6ee6bcd478d91481853b0b62bd0
shaon22 <shaonroy11@gmail.com>
2025-07-31 20:07:38 +0600

    updated

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,36 @@
-import Banner from "./Banner";
-import Nav from "../../components/Nav";
-import Sidenav from "../../components/Sidenav";
-import TrendingMovies from "./TrendingMovies";
-import TrendingSeries from "./TrendingSeries";
-import Blogs from "./Blogs";
-import Live from "./Live";
-import Footer from "../../components/Footer";
+import { BanknoteArrowDown } from "lucide-react"
+import Sidenav from "../../components/Sidenav"
+import Nav from "./Banner"
+import TrendingMovies from "./TrendingMovies"
+import TrendingSeries from "./TrendingSeries"
+import Blogs from "./Blogs"
+import Footer from "../../components/Footer"
+import Live from "./Live"
+
 
 const Home = () => {
-  return <div className=" min-h-screen home ">
-    
-            <div className="flex">
-              
-              <div className="w-[20%] shadow-2xl min-h-screen  fixed">
-                <Sidenav></Sidenav>
-                
-              </div>
-              <div className="w-[80%] ml-[300px]">
-                <Nav></Nav> 
-                <Banner></Banner>
-                <TrendingMovies></TrendingMovies>
-                <TrendingSeries></TrendingSeries>
-                <Blogs></Blogs>
-                <Live></Live>
-                <Footer></Footer>
-              </div>
-            
-            </div>
-           
-  </div>;
-};
+  return (
+    <div className="min-h-screen home">
+      <div className="flex flex-col md:flex-row">
+        {" "}
+        {/* Stacks vertically on small screens, horizontally on medium and up */}
+        {/* Sidenav container: hidden on small screens, fixed 300px width on medium and up */}
+        <div className="w-full md:w-[300px] shadow-2xl min-h-screen fixed hidden md:block">
+          <Sidenav />
+        </div>
+        {/* Main content area: full width on small screens, pushed by sidebar on medium and up */}
+        <div className="w-full md:ml-[300px] flex-1">
+          <Nav />
+          <BanknoteArrowDown />
+          <TrendingMovies />
+          <TrendingSeries />
+          <Blogs />
+          <Live />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
